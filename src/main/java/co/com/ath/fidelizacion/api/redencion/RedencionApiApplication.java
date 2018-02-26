3216c5b1b66b48cf76c58bb1c87d36a11bd7acf5
carlos.cardona <carlos.cardona@pragma.com.co>
2018-02-26 11:14:45 -0500

    fix
    Signed-off-by: carlos.cardona <carlos.cardona@pragma.com.co>

--- src/main/java/co/com/ath/fidelizacion/api/redencion/RedencionApiApplication.java
+++ src/main/java/co/com/ath/fidelizacion/api/redencion/RedencionApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RedencionApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedencionApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RedencionApiApplication.class, args);
+    }
 }
